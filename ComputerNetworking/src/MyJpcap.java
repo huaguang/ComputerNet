4f314661306bf8e3fc90cde2b3f5aff25111a23d
huaguang <huaguangyanmie@163.com>
2016-03-19 20:27:38 +0800

    核心功能完成

--- ComputerNetworking/src/MyJpcap.java
+++ ComputerNetworking/src/MyJpcap.java
@@ -13,20 +13,23 @@ import jpcap.packet.Packet;
 public class MyJpcap {
  public static void main(String[] args) throws Exception {
   args = new String[2];
-  args[0]="2";
-  args[1]="202.108.22.5";	//202.108.22.5 
-  if (args.length < 2) {
+  
+  //tracert 180.97.33.108		//追踪百度
+  args[0]="2";		//本机有四块虚拟网卡，可用的为第二块
+  args[1]="www.baidu.com";
+  
+ /* if (args.length < 2) {
    System.out.println("Usage: java Traceroute <device index (e.g., 0, 1..)> <target host address>");
    System.exit(0);
   } // initialize Jpcap
-  
+*/  
   //1.获取网卡实例
   NetworkInterface device = JpcapCaptor.getDeviceList()[Integer.parseInt(args[0])];
   //2.打开网卡实例
-  JpcapCaptor captor = JpcapCaptor.openDevice(device, 2000, false, 2000);
-  
+  JpcapCaptor captor = JpcapCaptor.openDevice(device, 2000, false, 8000);
   InetAddress thisIP = null;
-  
+  //InetAddress desIp=null;
+  //desIp=Inet4Address.getByName("www.baidu.com");
   //3、获取本网卡IPV4的地址
   for (NetworkInterfaceAddress addr : device.addresses)
    if (addr.address instanceof Inet4Address) {
@@ -34,11 +37,11 @@ public class MyJpcap {
     break;
    }
   // obtain MAC address of the default gateway
-  InetAddress pingAddr = InetAddress.getByName("www.baidu.com");
+  InetAddress pingAddr = InetAddress.getByName(args[1]);
   captor.setFilter("tcp and dst host " + pingAddr.getHostAddress(), true);
   byte[] gwmac = null;
   while (true) {
-   new URL("http://www.baidu.com").openStream().close();
+   new URL("http://"+args[1]).openStream().close();
    Packet ping = captor.getPacket();
    if (ping == null) {
     System.out
@@ -56,24 +59,24 @@ public class MyJpcap {
   icmp.seq = 100;
   icmp.id = 0;
   icmp.setIPv4Parameter(0, false, false, false, 0, false, false, false,
-    0, 0, 0, IPPacket.IPPROTO_ICMP, thisIP,
-    InetAddress.getByName(args[1]));
+    0, 0, 0, IPPacket.IPPROTO_ICMP, thisIP,InetAddress.getByName(args[1]));
   icmp.data = "data".getBytes();
   EthernetPacket ether = new EthernetPacket();
   ether.frametype = EthernetPacket.ETHERTYPE_IP;
   ether.src_mac = device.mac_address;
   ether.dst_mac = gwmac;
   icmp.datalink = ether;
+  icmp.hop_limit=1;
   captor.setFilter("icmp and dst host " + thisIP.getHostAddress(), true);
   JpcapSender sender = captor.getJpcapSenderInstance(); // JpcapSender
                 // sender=JpcapSender.openDevice(device);
   sender.sendPacket(icmp);
+  System.out.println("dstIp"+icmp.dst_ip+"\tsrcIp:"+icmp.src_ip);
   while (true) {
    ICMPPacket p = (ICMPPacket) captor.getPacket(); // System.out.println("received "+p);
    if (p == null) {
-    System.out.println("Timeout");
+    System.out.println(icmp.hop_limit+++":Timeout");
    } else if (p.type == ICMPPacket.ICMP_TIMXCEED) {
-   
     System.out.println(icmp.hop_limit + "(超时): " + p.src_ip);
     icmp.hop_limit++;
    } else if (p.type == ICMPPacket.ICMP_UNREACH) {
@@ -88,6 +91,20 @@ public class MyJpcap {
   }
  }
 }
+
+
+/*4(超时): /192.168.188.1
+5(超时): /218.2.182.33
+6(超时): /218.2.182.33
+7(超时): /218.2.182.33
+8(超时): /218.2.249.9
+9(超时): /58.213.51.33
+10(超时): /202.102.69.238
+11(超时): /202.102.69.238
+12(超时): /218.2.182.33
+13(超时): /202.102.69.238
+14(超时): /180.97.32.74
+15(回复): /180.97.33.107*/
 
 
 /*import java.net.InetAddress;
